9c1cf513b0fefe20eae0941d53d6b376fad8d508
Matt-Bryan <mbryan_379@yahoo.com>
2017-04-05 12:00:59 -0700

    after lab 4/5

---
+++ schoolsearch.java
@@ -0,0 +1,75 @@
+import java.util.*;
+import java.io.*;
+
+public class schoolsearch {
+
+	private static ArrayList<ArrayList<String>> data = new ArrayList<ArrayList<String>>();
+	
+	public static void fileParse() throws FileNotFoundException {
+		Scanner in = new Scanner(new File("students.txt"));
+		while (in.hasNextLine()) {
+			String line = in.nextLine();
+			String[] elements = line.split(",");
+			if (elements.length != 8)
+				continue;
+			ArrayList<String> row = new ArrayList<String>();
+			for (int i = 0; i < elements.length; i++)
+				row.add(elements[i]);
+			data.add(row);
+		}
+		in.close();
+	}
+
+	private static void infoPrint() {
+		int grade[] = new int[7];
+
+		for (ArrayList<String> current : data) {
+			grade[Integer.parseInt(current.get(2))]++;
+		}
+
+		for (int i = 0; i < 7; i++) {
+			System.out.println(i + ": " + grade[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			fileParse();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			System.exit(-1);
+		}
+		System.out.println("Command Menu:");
+		System.out.println("S[tudent]: <lastname> [B[us]]");
+		System.out.println("T[eacher]: <lastname>");
+		System.out.println("B[us]: <number>");
+		System.out.println("G[rade]: <number> [[H[igh]]|[L[ow]]]");
+		System.out.println("A[verage]: <number>");
+		System.out.println("I[nfo]");
+		System.out.println("Q[uit]");
+		Scanner in = new Scanner(System.in);
+		String input;
+		System.out.println("Enter command");
+		input = in.nextLine();
+		while (!input.equals("Q") && !input.equals("Quit")) {
+			String[] cmdParts = input.split(":");
+			if (cmdParts.length == 1) {
+				if (cmdParts[0].equals("I") || cmdParts[0].equals("Info")) {
+					infoPrint();
+				}
+				else {
+					System.out.println("Invalid Command: Syntax Error");
+				}
+			}
+			else if (cmdParts.length == 2) {
+				//Everything else
+			}
+			else {
+				System.out.println("Invalid Command: Syntax Error");
+			}
+			System.out.println("Enter command");
+			input = in.nextLine();
+		}
+	}
+}
